3bfbdf9168fd4c78fdf02c3cf5faed25b2570399
peterlee <lyfyulin1992@163.com>
2020-03-30 11:51:31 +0800

    'less-style'

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@
 */
 
 import React, {Component} from 'react'
+import {Button, message} from 'antd'
 
 export default class App extends Component{
+
+    handleClick = () => {
+        message.success("成功点击按钮！")
+    }
+
     render(){
         return (
-            <div>good job</div>
+            <div>
+            <h1>good job</h1>
+            <Button type="primary" onClick={this.handleClick}>确定</Button>
+            </div>
         )
     }
 }
